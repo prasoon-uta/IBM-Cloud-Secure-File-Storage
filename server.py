923dbeebf4d5f79387fb84c3ee79962c0fd382a7
prasoon-uta <prasoon1812@gmail.com>
2018-01-25 02:03:37 -0600

    updated setup to include flask package

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ import os
 
 import pyDes
 import swiftclient.client as swiftclient
-from flask import Flask, render_template, request, make_response
+import flask
 
 PORT = int(os.getenv('PORT', 80))
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.debug=True
 
 """
@@ -44,41 +44,41 @@ conn = swiftclient.Connection(
 
 @app.route('/',methods=['GET','POST'])
 def root():
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 
 @app.route('/createcontiner',methods=['GET','POST'])
 def createcontainer():
-    container_name =request.form['containername']
+    container_name =flask.request.form['containername']
     conn.put_container(container_name)
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 @app.route('/displaycontainers',methods=["GET",'POST'])
 def displaycontainers():
     container_list=[]
     for container in conn.get_account()[1]:
         container_list.append(container['name'])
-    return render_template("index.html",container_list=container_list)
+    return flask.render_template("index.html", container_list=container_list)
 
 @app.route('/deletecontainer',methods=["GET",'POST'])
 def deletecontainers():
-    container_name=request.form['getdeletecontainer']
+    container_name=flask.request.form['getdeletecontainer']
     conn.delete_container(container_name)
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 @app.route('/upload_file',methods=['GET','POST'])
 def upload():
 
-    if request.method== 'POST':
-        container_name=request.form['uploadcontainername']
-        f=request.files['file']
+    if flask.request.method== 'POST':
+        container_name=flask.request.form['uploadcontainername']
+        f=flask.request.files['file']
         data = f.stream.read()
         conn.put_object(container_name,
                         f.filename,
                         contents=encod(data,'qweasdzx'),
                         content_type='text/plain')
 
-        return render_template("index.html",msg="File uploaded suceesfully")
+        return flask.render_template("index.html", msg="File uploaded suceesfully")
 
 def encod(data, password):
     k = pyDes.des(password, pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
@@ -95,43 +95,43 @@ def decod(data, password):
 
 @app.route('/download_file',methods=['GET','POST'])
 def download():
-        container_name=request.form['downloadcontainername']
-        filenme=request.form['downloadingfile']
+        container_name=flask.request.form['downloadcontainername']
+        filenme=flask.request.form['downloadingfile']
         obj = conn.get_object(container_name, filenme)
         file_contents = obj[1]
         actual_file=decod(file_contents,'qweasdzx')
 
-        if request.method == 'POST' :
-            response = make_response(actual_file)
+        if flask.request.method == 'POST' :
+            response = flask.make_response(actual_file)
             response.headers["Content-Disposition"] = "attachment; filename=%s"%filenme
             return response
 
 
 @app.route('/displaydata',methods=['GET','POST'])
 def displayingdata():
-    container_name = request.form['containnmae']
-    object_name=request.form['objecname']
+    container_name = flask.request.form['containnmae']
+    object_name=flask.request.form['objecname']
     data=conn.get_object(container_name,object_name)
     file_contents=data[1]
-    return render_template("index.html",file_contents=file_contents)
+    return flask.render_template("index.html", file_contents=file_contents)
 
 
 @app.route('/listfiles',methods=['GET','POST'])
 def displayfilesfromcontainer():
-    container_name=request.form['conname']
+    container_name=flask.request.form['conname']
     object_list=[]
     for data in conn.get_container(container_name)[1]:
         object_list.append(data['name'])
         object_list.append(data['bytes'])
         object_list.append(data['last_modified'])
-    return render_template("index.html",object_list=object_list)
+    return flask.render_template("index.html", object_list=object_list)
 
 @app.route("/deleteobjects",methods=['GET','POST'])
 def deleteobjects():
-    container_name=request.form['deleteconater']
-    obj_name=request.form['objname']
+    container_name=flask.request.form['deleteconater']
+    obj_name=flask.request.form['objname']
     conn.delete_object(container_name, obj_name)
-    return render_template("index.html")
+    return flask.render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(PORT), threaded=True, debug=False)
